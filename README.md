998e754b716ab1ae51f66c0b9dcac3bbb8b875fc
Kristupas Antanavicius <kristupas.antanavicius@sbdigital.lt>
2020-01-22 12:09:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Features
+Analog of buildtools/clang_format/script/clang-format-sublime.py
+
+Finds depot_tools using 'where python' or 'which python' and executes clang_format.py
+
+Restores scroll position after formatting
+
+## Requirements
+
+depot_tools in your path and the file being formatted has to be in a Chromium tree
---
+++ src/extension.ts
@@ -0,0 +1,66 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as child from 'child_process';
+import * as path from 'path';
+import * as process from 'process';
+import * as vscode from 'vscode';
+
+function formatCode() {
+  let [findPython, pythonName] = process.platform == 'win32' ?
+      ['where python', 'python.bat'] :
+      ['which python', 'python'];
+  let toolsPath = path.dirname(child.execSync(findPython).toString());
+
+  let python = path.normalize(toolsPath + '/' + pythonName);
+  let clangFormat = path.normalize(toolsPath + '/clang_format.py');
+
+  let args = [];
+  args.push(clangFormat);
+  args.push('-style', 'file');
+
+  let editor = vscode.window.activeTextEditor as vscode.TextEditor;
+
+  editor.selections.forEach(selection => {
+    let start = editor.document.offsetAt(editor.selection.start);
+    let end = editor.document.offsetAt(editor.selection.end);
+
+    args.push('-offset', Math.min(start, end).toString());
+    args.push('-length', Math.abs(end - start).toString());
+    args.push('-assume-filename', editor.document.fileName);
+  });
+
+  let opts = {
+    cwd: path.dirname(editor.document.fileName),
+    encoding: 'utf-8',
+    input: editor.document.getText(),
+  };
+
+  let result = child.spawnSync(python, args, opts);
+
+  let previousRange = editor.visibleRanges[0];
+
+  editor.edit(editBuilder => {
+    let invalidRange =
+        new vscode.Range(0, 0, editor.document.lineCount, 0);
+    let fullRange = editor.document.validateRange(invalidRange) || invalidRange;
+    editBuilder.replace(fullRange, result.stdout.toString());
+  });
+
+  editor.revealRange(previousRange);
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  let disposable =
+      vscode.commands.registerCommand('extension.chromium-clangformat', () => {
+        try {
+          formatCode();
+        } catch (e) {
+          console.log(e);
+        }
+      });
+
+  context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
